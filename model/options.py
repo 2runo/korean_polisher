c25fdd1edfb1c339d6dafa77f7d4fed2ac760022
2runo <2runo04@gmail.com>
2020-10-08 19:41:25 +0900

    transformer model update

---
+++ model/options.py
@@ -0,0 +1,12 @@
+vocab_size = 5000
+num_layers = 4
+d_model = 128
+dff = 512
+num_heads = 8
+
+input_vocab_size = vocab_size + 2
+target_vocab_size = vocab_size + 2
+dropout_rate = 0.1
+
+EPOCHS = 20
+checkpoint_path = "./checkpoints"
